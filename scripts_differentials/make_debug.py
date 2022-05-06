87bea18486f61cfac6d991abb5fda76ff0bbb12d
Massimiliano Galli <massimiliano.galli@cern.ch>
2022-05-06 10:06:45 +0200

    script to make debug dir

---
+++ scripts_differentials/make_debug.py
@@ -0,0 +1,55 @@
+import sys
+import yaml
+import json
+
+
+def main():
+    """
+    """
+    if len(sys.argv) != 2:
+        print("Usage: eft2obs_to_channels.py <input_file>")
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+
+    with open(input_file, "r") as f:
+        channel_file = yaml.safe_load(f)
+
+    output_dict = {}
+
+    for channel, file in channel_file.items():
+        output_dict[channel] = {}
+        with open(file, "r") as f:
+            eft2obs_dct = json.load(f)
+        edges_list = eft2obs_dct["edges"]
+        bins = eft2obs_dct["bins"]
+        for edges, deviations in zip(edges_list, bins):
+            bin_name = "{}_{}".format(
+                str(edges[0]).replace(".", "p"), str(edges[1]).replace(".", "p")
+            )
+            output_dict[channel][bin_name] = {}
+            for dev in deviations:
+                val = dev[0]
+                unc = dev[1]
+                pars = dev[2:]
+                if len(pars) == 1:
+                    output_dict[channel][bin_name]["A_{}".format(pars[0])] = 10.1
+                    output_dict[channel][bin_name]["u_A_{}".format(pars[0])] = 0.1
+                elif len(pars) == 2:
+                    if pars[0] == pars[1]:
+                        output_dict[channel][bin_name]["B_{}_2".format(pars[0])] = 10.1
+                        output_dict[channel][bin_name]["u_B_{}_2".format(pars[0])] = 0.1
+                    else:
+                        output_dict[channel][bin_name][
+                            "B_{}_{}".format(pars[0], pars[1])
+                        ] = 10.1
+                        output_dict[channel][bin_name][
+                            "u_B_{}_{}".format(pars[0], pars[1])
+                        ] = 0.1
+
+    with open("channels.json", "w") as f:
+        json.dump(output_dict, f, indent=4)
+
+
+if __name__ == "__main__":
+    main()
